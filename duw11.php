9026d98ebb98cebf160192cf3951497959710eec
Bui Huy Duc <150815452+hducdev@users.noreply.github.com>
2025-06-21 20:58:27 +0700

    Update duw11.php

--- duw11.php
+++ duw11.php
@@ -30,7 +30,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2y$10$PK1PFGyTHOFcR5.n4U1P8ujEHgSOgYhHAJncjtlvBr.OybvmwmENm'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2y$10$HlwCsnSrEmr/28P/nvwl/OLW8TQ.NPomTtSQeHeT.4Jx.cLpxP6ca'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;
